a10a87975be9cad6a99791b805a419d298c43c11
Jansen <martijn.e.jansen@rabobank.nl>
2019-11-07 14:23:24 +0100

    Change something insanely important

--- src/main/java/com/demo/graphql/model/User.java
+++ src/main/java/com/demo/graphql/model/User.java
@@ -1,14 +1,10 @@
 package com.demo.graphql.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Data
-@RequiredArgsConstructor
-@AllArgsConstructor
 public class User {
     private final String userType;
     private final int age;
